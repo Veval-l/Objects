d8be21656134d0edc5d3e576af632e675b121ed6
Oseeva <fselti1168rus@gmail.com>
2023-11-22 21:42:50 +0700

    Добавила класс "Автор"

---
+++ src/Author.java
@@ -0,0 +1,10 @@
+public class Author {
+    String name;
+    String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,18 @@ public class Main {
         System.out.println("book1.nameBook = " + book1.nameBook);
         System.out.println("book1.yearOfIssues = " + book1.yearOfIssues);
 
-        Book book2 = new Book("Гарри Поттер", 200);
+        Author author1 = new Author("Мосян", "Тунсю");
+        System.out.println("Автор: " + author1.name + " " + author1.surname);
+
+        System.out.println();
+
+        Book book2 = new Book("Гарри Поттер", 2000);
         System.out.println("book2.nameBook = " + book2.nameBook);
         System.out.println("book2.yearOfIssues = " + book2.yearOfIssues);
+
+        Author author2 = new Author("Джоан", "Роулинг");
+        System.out.println("Автор: " + author2.name + " " + author2.surname);
+
 
         System.out.println();
 
