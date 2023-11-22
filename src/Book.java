86f6574ade4cdee864c1267d97851d55cf3ab692
Oseeva <fselti1168rus@gmail.com>
2023-11-22 21:21:00 +0700

    Конструктор для двух книг

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+
+    String nameBook;
+    int yearOfIssues;
+
+    public Book(String nameBook, int yearOfIssues) {
+        this.nameBook = nameBook;
+        this.yearOfIssues = yearOfIssues;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
-
-
     public static void main(String[] args) {
-
-        task1();
-
-    }
-
-    static public void task1() {
         System.out.println("Задача 1");
+        System.out.println();
 
+        Book book1 = new Book("Магистр темного пути", 2022);
+        System.out.println("book1.nameBook = " + book1.nameBook);
+        System.out.println("book1.yearOfIssues = " + book1.yearOfIssues);
+
+        Book book2 = new Book("Гарри Поттер", 200);
+        System.out.println("book2.nameBook = " + book2.nameBook);
+        System.out.println("book2.yearOfIssues = " + book2.yearOfIssues);
 
         System.out.println();
+
     }
 }
